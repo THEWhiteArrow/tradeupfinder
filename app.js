1469ff10d46937ed149d4016b07bec72890040aa
WhiteArro <damitrafi6@gmail.com>
2021-03-21 14:06:14 +0100

    working checker v2

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
+// const mongoose = require('mongoose');
+
+// mongoose.connect(dbUrl, {
+//    useNewUrlParser: true,
+//    useCreateIndex: true,
+//    useUnifiedTopology: true,
+//    useFindAndModify: false
+// });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//    console.log("Database connected");
+// });
+
 
 const app = express();
 app.use(express.json({ extended: true }));
@@ -11,10 +25,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const skins = ['AWP BOOM', 'AK-47 Wasteland Rebel']
-let i = 0
+const skins = ['AWP BOOM', 'AK-47 Wasteland Rebel', 'Glock-18 Fade'];
+let i = 0;
+
+
+
+
+
+
+
+
 
 app.get('/', async (req, res) => {
+
    const data = await getSkinPage(`https://steamcommunity.com/market/listings/730/${toName(`${skins[i]}`, 'Factory New')}`);
    res.render('home', { data });
 });
@@ -23,6 +46,7 @@ app.get('/value', async (req, res) => {
    const { price } = req.query;
    console.log(`${skins[i]} - ${price}`);
    i += 1;
+   // if (i < skins.length - 1) i += 1;
 
    res.json('completely gatherd price')
 });
--- public/steamapi.js
+++ public/steamapi.js
@@ -1,12 +1,15 @@
 const sendPrice = async (price) => {
    try {
 
+
+
       const url = `http://localhost:3000/value?price=${price}`;
       const res = await fetch(url);
       const data = await res.json();
       // let dataString = await String(data)
       console.log(data);
       window.location = 'http://localhost:3000/';
+
    } catch (e) {
       console.log(e)
    }
@@ -14,5 +17,6 @@ const sendPrice = async (price) => {
 }
 
 const price = document.querySelector('span.market_listing_price').innerText;
+console.log(price);
 sendPrice(price);
 
