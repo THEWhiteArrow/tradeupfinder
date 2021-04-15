56efdff54bf1de12e36e86bdca72f2d0ea29baf7
WhiteArro <damitrafi6@gmail.com>
2021-04-15 09:19:50 +0200

    fixed issue with displaying saved searches

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ const ExpressError = require('./utils/ExpressError');
 
 
 module.exports.isCorrectServer = (req, res, next) => {
-   if (res.locals.server != 'local' && req.query.cookie != 'readLast') {
+   if (res.locals.server != 'local' && req.query.cookie != 'display') {
       throw new ExpressError("Access Denied. Those servers are not meant to conduct such calculations", 401);
    }
    next();
