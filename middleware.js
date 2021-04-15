cd8e5eeb6f45d7564fd6932785e06ae417a5ab5c
WhiteArro <damitrafi6@gmail.com>
2021-04-15 09:23:20 +0200

    fixed unseen previous bug

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ const ExpressError = require('./utils/ExpressError');
 
 
 module.exports.isCorrectServer = (req, res, next) => {
-   if (res.locals.server != 'local' && req.query.cookie != 'display') {
+   if (res.locals.server != 'local' && req.query.action != 'display') {
       throw new ExpressError("Access Denied. Those servers are not meant to conduct such calculations", 401);
    }
    next();
