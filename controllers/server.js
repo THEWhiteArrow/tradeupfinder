c02e4494608d2b857ac81e630702622b92982139
WhiteArro <damitrafi6@gmail.com>
2021-07-07 16:21:40 +0200

    fixed repetitous validating outer server response

--- controllers/server.js
+++ controllers/server.js
@@ -2,8 +2,12 @@ const ServerInfo = require('../models/serverInfoModel');
 
 module.exports.validOuterServerAction = async (req, res) => {
    const serverInfo = await ServerInfo.findOne({})
-
-   if (serverInfo.outerServerInfo.valid) {
+   if (serverInfo.outerServerInfo.valid == null) {
+      req.flash('success', 'NOTHING happened since last time checking. You are just repeating validating !')
+      res.redirect('/skins')
+   }
+   else if (serverInfo.outerServerInfo.valid) {
+      await ServerInfo.findOneAndUpdate({}, { "outerServerInfo.valid": null })
       req.flash('success', 'Everything on the outside went correctly!')
       res.redirect('/skins')
    } else {
